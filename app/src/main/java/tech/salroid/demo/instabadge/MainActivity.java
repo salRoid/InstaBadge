eb3c32ad8dc6d218bae0b60c923bfbf2ff9e42d0
sajal gupta <gupta.sajal631@gmail.com>
2017-02-18 16:50:40 +0530

    initial commit

---
+++ app/src/main/java/tech/salroid/demo/instabadge/MainActivity.java
@@ -0,0 +1,51 @@
+package tech.salroid.demo.instabadge;
+
+import android.graphics.Canvas;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_home_white_24dp));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_favorite_white_24dp));
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.ic_perm_identity_white_24dp));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/tech/salroid/demo/instabadge/PagerAdapter.java
@@ -0,0 +1,37 @@
+package tech.salroid.demo.instabadge;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                FragmentOne tab1 = new FragmentOne();
+                return tab1;
+            case 1:
+                FragmentTwo tab2 = new FragmentTwo();
+                return tab2;
+            case 2:
+                Fragmentthree tab3 = new Fragmentthree();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}

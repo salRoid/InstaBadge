cf87dc8c6e96f9c4db79f47d267029c64d350a5b
webianks <webianks@gmail.com>
2017-02-19 01:48:23 +0530

    Formatted code and added a stylable-attr

--- app/src/main/java/tech/salroid/demo/instabadge/InstaBadge.java
+++ app/src/main/java/tech/salroid/demo/instabadge/InstaBadge.java
@@ -1,6 +1,7 @@
 package tech.salroid.demo.instabadge;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.LayerDrawable;
@@ -17,58 +18,84 @@ import android.widget.TextView;
 
 public class InstaBadge extends LinearLayout {
 
+    private Context context;
+    private int dpAsPixels;
+    private LinearLayout outer_container;
+    private float scale;
+    private ImageView badge_icon;
+    private TextView badge_text;
+    private LinearLayout bottom_arrow;
+
+    private String BADGE_COLOR = "026A69A";
+
     public InstaBadge(Context context) {
         super(context);
-        init(context);
+        this.context = context;
+        init();
     }
 
     public InstaBadge(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        this.context = context;
+
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.InstaBadge, 0, 0);
+        initAttributes(typedArray);
+        typedArray.recycle();
+
+        init();
     }
 
     public InstaBadge(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
+        this.context = context;
+
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.InstaBadge, 0, 0);
+        initAttributes(typedArray);
+        typedArray.recycle();
+
+        init();
     }
 
-    private void init(Context context) {
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-        setOrientation(VERTICAL);
-        setLayoutParams(params);
+    private void initAttributes(TypedArray typedArray) {
 
-        LinearLayout outer_container = new LinearLayout(context);
-        outer_container.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 2f));
-        outer_container.setOrientation(LinearLayout.HORIZONTAL);
-        outer_container.setGravity(Gravity.CENTER_HORIZONTAL);
+        String text_color = typedArray.getString(R.styleable.InstaBadge_setColor);
+/*
+        if (text_color!=null && text_color.length()>0)
+            BADGE_COLOR = Integer.parseInt(text_color);*/
 
-        float scale = getResources().getDisplayMetrics().density;
-        int dpAsPixels = (int) (13 * scale + 0.5f);
-        outer_container.setPadding(dpAsPixels, dpAsPixels, dpAsPixels, dpAsPixels);
+    }
 
-        ImageView badge_icon = new ImageView(context);
-        dpAsPixels = (int) (16 * scale + 0.5f);
-        LayoutParams badge_params = new LinearLayout.LayoutParams(dpAsPixels, dpAsPixels, 1f);
-        dpAsPixels = (int) (2 * scale + 0.5f);
-        badge_params.setMargins(0, 0, dpAsPixels, 0);
-        badge_params.gravity = Gravity.CENTER;
-        badge_icon.setLayoutParams(badge_params);
-        badge_icon.setImageDrawable(getResources().getDrawable(R.drawable.ic_favorite_white_24dp));
+    private void init() {
 
-        TextView badge_text = new TextView(context);
-        LayoutParams text_params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f);
-        text_params.setMargins(dpAsPixels, 0, 0, 0);
+        setupMainLayout();
+        setupBadgeImage();
+        setupBadgeText();
 
-        badge_text.setText("6");
-        badge_text.setGravity(Gravity.CENTER);
-        badge_text.setTextColor(Color.WHITE);
-        badge_text.setLayoutParams(text_params);
+        addImageAndText();
+        setupBottomArrow();
 
-        outer_container.setBackgroundResource(R.drawable.background);
-        outer_container.addView(badge_icon);
-        outer_container.addView(badge_text);
-        
-        LinearLayout bottom_arrow = new LinearLayout(context);
+        setupBadgeBackgroundColors();
+
+    }
+
+    private void setupBadgeBackgroundColors() {
+
+        GradientDrawable gd = (GradientDrawable) outer_container.getBackground();
+        gd.setColor(Color.parseColor(BADGE_COLOR));
+
+        LayerDrawable layers = (LayerDrawable) bottom_arrow.getBackground();
+        RotateDrawable rotate = (RotateDrawable) layers.findDrawableByLayerId(R.id.grad);
+        GradientDrawable drawable = (GradientDrawable) rotate.getDrawable();
+        drawable.setColor(Color.parseColor(BADGE_COLOR));
+
+    }
+
+    private void setupBottomArrow() {
+
+
+        bottom_arrow = new LinearLayout(context);
         dpAsPixels = (int) (20 * scale + 0.5f);
         LayoutParams arrow_params = new LinearLayout.LayoutParams(dpAsPixels, dpAsPixels, 1f);
         arrow_params.gravity = Gravity.CENTER_HORIZONTAL;
@@ -79,14 +106,60 @@ public class InstaBadge extends LinearLayout {
         addView(outer_container);
         addView(bottom_arrow);
 
-        GradientDrawable gd = (GradientDrawable) outer_container.getBackground();
-        gd.setColor(Color.parseColor("#26A69A"));
+    }
 
-        LayerDrawable layers = (LayerDrawable) bottom_arrow.getBackground();
-        RotateDrawable rotate = (RotateDrawable) layers.findDrawableByLayerId(R.id.grad);
-        GradientDrawable drawable = (GradientDrawable) rotate.getDrawable();
-        drawable.setColor(Color.parseColor("#26A69A"));
+    private void addImageAndText() {
+
+        outer_container.setBackgroundResource(R.drawable.background);
+        outer_container.addView(badge_icon);
+        outer_container.addView(badge_text);
+    }
+
+    private void setupBadgeText() {
+
+        badge_text = new TextView(context);
+        LayoutParams text_params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f);
+        text_params.setMargins(dpAsPixels, 0, 0, 0);
+
+        badge_text.setText("6");
+        badge_text.setGravity(Gravity.CENTER);
+        badge_text.setTextColor(Color.WHITE);
+        badge_text.setLayoutParams(text_params);
 
     }
+
+    private void setupBadgeImage() {
+
+        badge_icon = new ImageView(context);
+        dpAsPixels = (int) (16 * scale + 0.5f);
+        LayoutParams badge_params = new LinearLayout.LayoutParams(dpAsPixels, dpAsPixels, 1f);
+        dpAsPixels = (int) (2 * scale + 0.5f);
+        badge_params.setMargins(0, 0, dpAsPixels, 0);
+        badge_params.gravity = Gravity.CENTER;
+        badge_icon.setLayoutParams(badge_params);
+        badge_icon.setImageDrawable(getResources().getDrawable(R.drawable.ic_favorite_white_24dp));
+
+    }
+
+    private void setupMainLayout() {
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+        setOrientation(VERTICAL);
+        setLayoutParams(params);
+
+        outer_container = new LinearLayout(context);
+        outer_container.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT, 2f));
+        outer_container.setOrientation(LinearLayout.HORIZONTAL);
+        outer_container.setGravity(Gravity.CENTER_HORIZONTAL);
+
+        scale = getResources().getDisplayMetrics().density;
+        dpAsPixels = (int) (13 * scale + 0.5f);
+        outer_container.setPadding(dpAsPixels, dpAsPixels, dpAsPixels, dpAsPixels);
+
+    }
+
+
 
 }
